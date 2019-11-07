c75889424a19bd4b6383559c57993053b52bcb07
tory37@gmail.com <tory37@gmail.com>
2019-11-07 00:22:44 -0600

    Made some changes

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -9,33 +9,38 @@ public class AudioManager : MonoBehaviour
     #region Serialized Fields
     [Header("Starting Values")]
     [SerializeField] private const int seedValue = 69;
-    [SerializeField] private float pitchValue = 1.0f;
     
     //Might eventually check for repetitive random pitches
     //[SerializeField] private float previousPitch;
 
-    [SerializeField] private AudioSource audioSource;
-    [Header("Balloon Pop (not really)")]
-    [SerializeField] private AudioClip audioClip;
+    [Header("Components")]
+    [SerializeField] private AudioSource audioSource = null;
+
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip clipBalloonPop = null;
     #endregion
 
-     #region Initialization
+    // This doesn't need to be serialized, but we will leave for testing purposes, to easily see the pitch each time in the inspector
+    [SerializeField] private float pitchValue = 1.0f;
+
+    #region Initialization
     void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(this.gameObject);
         } 
         else
         {
             Destroy(this.gameObject);
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();  
-        audioSource.clip = audioClip;
+        instance.audioSource = GetComponent<AudioSource>();  
         Random.InitState(seedValue);
     }
     #endregion
@@ -44,23 +49,42 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        audioSource.pitch = pitchValue; 
-
         //test trigger for audio clip
         if (Input.GetKeyDown("return")){
-            audioSource.Play();
-            pitchValue = RandomPitch();
-            Debug.Log(pitchValue);
+            PlayBalloonPop();
         }
     }
     #endregion
 
     #region Random Pitch Shifter
-    private  float RandomPitch(){
+    private  float getRandomPitch(){
         float newPitch = Random.value;
         //scales 0-1 to .75-1.25
         newPitch = (((newPitch - 0.0f) * (1.25f - 0.75f)) / (1.0f - 0.0f)) + 0.75f;
         return newPitch;
+    }
+    #endregion
+
+    #region Triggers
+
+    // This will be where you shift actually apply the shift
+    private void playShiftedClip(AudioClip clip)
+    {
+        instance.audioSource.clip = clip;
+        instance.audioSource.Play();
+        instance.pitchValue = instance.getRandomPitch();
+        Debug.Log("Pitch Value: " + pitchValue);
+    }
+
+    private void playUnshiftedClip(AudioClip clip)
+    {
+        instance.audioSource.clip = clip;
+        instance.audioSource.Play();
+    }
+
+    public static void PlayBalloonPop()
+    {
+        instance.playShiftedClip(instance.clipBalloonPop);
     }
     #endregion
 }
